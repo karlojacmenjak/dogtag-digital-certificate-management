e92868940d54bea9738f9815b1023c8a645e8a5c
kjacmenja21 <karlojacmenjak@gmail.com>
2025-03-24 15:26:10 +0100

    Add routing

--- frontend/src/routes/App.tsx
+++ frontend/src/routes/App.tsx
@@ -1,5 +1,6 @@
 import type { Component } from "solid-js";
-import { Username } from "./components/username";
+import { Password } from "../components/password";
+import { Username } from "../components/username";
 
 const App: Component = () => {
   return (
@@ -8,22 +9,22 @@ const App: Component = () => {
         <figure>
           <img
             src="https://images.unsplash.com/photo-1454117096348-e4abbeba002c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Shoes"
+            alt="Card Image"
           />
         </figure>
         <div class="card-body bg-neutral-600">
-          <h2 class="card-title">Card Title</h2>
+          <h2 class="card-title">Certificate Authority</h2>
           <p>
-            A card component has a figure, a body part, and inside body there
-            are title and actions parts
+            Welcome to Certificate Authority management platform! To begin
+            working with the platform, please login.
           </p>
-          <div class="card-actions">
+          <div class="card-actions justify-evenly">
             <Username />
           </div>
-          <div class="card-actions">
-            <Username />
+          <div class="card-actions justify-evenly">
+            <Password />
           </div>
-          <div class="card-actions justify-end">
+          <div class="card-actions justify-end p-8">
             <button class="btn btn-primary">Login</button>
           </div>
         </div>
